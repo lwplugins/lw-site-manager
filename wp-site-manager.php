028acfc242f966d923bb516ca68a226250369dd8
trueqap <trueqap@gmail.com>
2026-01-13 20:14:32 +0100

    Release v1.0.1

--- wp-site-manager.php
+++ wp-site-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Site Manager
  * Plugin URI: https://github.com/trueqap/wp-site-manager
  * Description: WordPress Site Manager using Abilities API - Full site maintenance via AI/REST
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires at least: 6.9
  * Requires PHP: 8.0
  * Author: trueqap
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Plugin constants
-define( 'WPSM_VERSION', '1.0.0' );
+define( 'WPSM_VERSION', '1.0.1' );
 define( 'WPSM_PLUGIN_FILE', __FILE__ );
 define( 'WPSM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WPSM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
